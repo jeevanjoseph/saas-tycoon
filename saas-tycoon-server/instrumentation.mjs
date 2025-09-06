b0a3da77017c007de569de002359b37dda9682c6
Jeevan Joseph <jeevan.g@gmail.com>
2025-09-06 12:20:43 -0700

    Integrating Prometheus exporter, OTLP Trace relay and Service Monitor

--- saas-tycoon-server/instrumentation.mjs
+++ saas-tycoon-server/instrumentation.mjs
@@ -4,6 +4,8 @@ import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
+import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
+
 import {
     PeriodicExportingMetricReader,
     ConsoleMetricExporter,
@@ -20,17 +22,22 @@ const sdk = new NodeSDK({
         [ATTR_SERVICE_NAME]: 'saas-tycoon-server',
         [ATTR_SERVICE_VERSION]: '0.1.0',
     }),
-    traceExporter: new ConsoleSpanExporter(),
-    // traceExporter: new OTLPTraceExporter({
-    // url: '<your-otlp-endpoint>/v1/traces', // optional - default url is http://localhost:4318/v1/traces
-    // headers: {}, // optional - collection of custom headers to be sent with each request, empty by default
-    metricReader: new PeriodicExportingMetricReader({
-        exporter: new ConsoleMetricExporter(),
-        // exporter: new OTLPMetricExporter({
-        //     url: '<your-otlp-endpoint>/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
-        //     headers: {}, // an optional object containing custom headers to be sent with each request
-        //     concurrencyLimit: 1, // an optional limit on pending requests
-        // }),
+    //traceExporter: new ConsoleSpanExporter(),
+    traceExporter: new OTLPTraceExporter({
+        url: 'http://unified-relay-traces.spectra-platform.svc.cluster.local:4318/v1/traces', // optional - default url is http://localhost:4318/v1/traces
+        headers: {}, // optional - collection of custom headers to be sent with each request, empty by default
+    }),
+    // metricReader: new PeriodicExportingMetricReader({
+    //     exporter: new ConsoleMetricExporter(),
+    //     // exporter: new OTLPMetricExporter({
+    //     //     url: '<your-otlp-endpoint>/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+    //     //     headers: {}, // an optional object containing custom headers to be sent with each request
+    //     //     concurrencyLimit: 1, // an optional limit on pending requests
+    //     // }),
+    // }),
+    metricReader: new PrometheusExporter({
+        port: 9464,
+        endpoint: '/metrics',
     }),
     instrumentations: [getNodeAutoInstrumentations()],
 });
